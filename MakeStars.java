97fdf23d30e899a73ffbfad1909943a75c7f8213
Alex Conkey <aconkey20@stuy.edu>
2021-01-06 09:07:55 -0500

    set up for loop inside

--- MakeStars.java
+++ MakeStars.java
@@ -4,7 +4,12 @@ public class MakeStars {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     while (scan.hasNextLine()){
-      
+      String currentLine = scan.nextLine();
+      String convertedLine = "";
+      for(int i = 0; i < currentLine.length(); i++){
+        if currentLine.charAt(i) != ' ';
+        
+      }
     }
   }
 }
