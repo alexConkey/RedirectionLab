153bd4a0bb4c6d7b0281950973d0bd72189a80ba
Alex Conkey <aconkey20@stuy.edu>
2021-01-06 09:47:34 -0500

    finished simple

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,13 @@
 public class PigLatin {
-  public static String pigLatinSimple(String s){}
+  public static String pigLatinSimple(String s){
+    if ("AEIOUaeiou".indexOf(s.charAt(0)) != -1) {
+      return (s.substring(0, s.length())+ "hay");
+    }else if(s.length() >= 2){
+      return (s.substring(1, s.length()) + s.substring(0, 1) + "ay");
+    }else{
+      return (s.substring(0, 1) + "ay");
+    }
+  }
 
   public static String pigLatin(String s){}
 
