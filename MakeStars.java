20f1079c7d24ea7149fce79af431ebc2f2b5e276
Alex Conkey <aconkey20@stuy.edu>
2021-01-06 08:59:17 -0500

    set up scanner and while loop to iterate through the lines

--- MakeStars.java
+++ MakeStars.java
@@ -1,6 +1,10 @@
-import java.util.Scanner
+import java.util.*;
 
 public class MakeStars {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    while (scan.hasNextLine()){
+      
+    }
   }
 }
