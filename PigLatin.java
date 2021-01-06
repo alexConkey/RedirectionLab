0c1c99df636d264a69dd6f57a6ce0664e3efefdd
Alex Conkey <aconkey20@stuy.edu>
2021-01-06 10:40:05 -0500

    finished pigLatinBest

--- PigLatin.java
+++ PigLatin.java
@@ -26,5 +26,30 @@ public class PigLatin {
     }
   }
 
-  public static String pigLatinBest(String s){}
+  public static String pigLatinBest(String s){
+    ArrayList<String> diaphrams = Arrays.asList({"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"});
+    String punctuation = "";
+    String newS = "";
+    for (int i = 0; i < s.length(); i++){
+      if ("abcdefghijklmnopqrstuvwxyz1234567890".indexOf(s.toLowerCase().charAt(i)) != -1){
+        newS += s.charAt(i);
+      }else{
+        punctuation += s.charAt(i)
+      }
+    }
+    s = newS;
+    if ("abcdefghijklmnopqrstuvwxyz".indexOf(s.toLowerCase().charAt(0)) == -1){
+      return s;
+    }else if ("AEIOUaeiou".indexOf(s.charAt(0)) != -1) {
+      return (s.substring(0, s.length())+ "hay").toLowerCase() + punctuation;
+    }else if (s.length() >= 3 && (diaphrams.indexOf(s.substring(0,2)) != -1)){
+      return (s.substring(2, s.length()) + s.substring(0, 2) + "ay").toLowerCase() + punctuation;
+    }else if (s.length() >= 2 && (diaphrams.indexOf(s.substring(0,2)) != -1)){
+      return (s.substring(0, 2) + "ay").toLowerCase() + punctuation;
+    }else if(s.length() >= 2){
+      return (s.substring(1, s.length()) + s.substring(0, 1) + "ay").toLowerCase() + punctuation;
+    }else{
+      return (s.substring(0, 1) + "ay").toLowerCase() + punctuation;
+    }
+  }
 }
