6634226f7e53eddbd870d01fa59022a59acafead
Alex Conkey <aconkey20@stuy.edu>
2021-01-06 09:13:43 -0500

    made for loop convert lines to *s

--- MakeStars.java
+++ MakeStars.java
@@ -4,12 +4,17 @@ public class MakeStars {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     while (scan.hasNextLine()){
+      boolean space = false;
       String currentLine = scan.nextLine();
       String convertedLine = "";
       for(int i = 0; i < currentLine.length(); i++){
-        if currentLine.charAt(i) != ' ';
-        
+        if (currentLine.charAt(i) != ' '){
+          convertedLine += "*";
+        }else{
+          convertedLine += " ";
+        }
       }
+      System.out.println(convertedLine);
     }
   }
 }
